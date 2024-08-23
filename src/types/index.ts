d73f0728edd6bb49921990e2aa7f667e020a375a
Evgeny Lukin <lukin@usota.ru>
2024-08-23 16:37:26 +0300

    Подготовка документации

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+export interface ICardCatalog {
+    cards: ICard[];
+    preview: string | null;
+}
+
+export interface ICard {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number;
+}
+
+export interface IOrder {
+    payment: string;
+    email: string;
+    phone: string;
+    address: string;
+    total: number;
+    items: ICard[];
+}
+
+export interface IBasket {
+    basketList: TCardCompact[];
+    getTotal(): number;
+}
+
+export type TCardCompact = Pick<ICard, 'title' | 'price'>
+
+export interface ICheckOut {
+    checkValidation(data: Record<keyof TCheckOut, string>): boolean;
+}
+
+export interface IContacts {
+    checkValidation(data: Record<keyof TContacts, string>): boolean;
+}
+
+export type TCheckOut = Pick<IOrder, 'payment' | 'address'>
+
+export type TContacts = Pick<IOrder, 'email' | 'phone'>
